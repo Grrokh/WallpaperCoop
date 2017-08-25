de0865503692951a6bd87ba5a4a0fa0e7e9c6ab3
Grrokh <grrokh@gmail.com>
2017-08-25 22:10:25 +0300

    added PPU normalaizer

---
+++ Assets/Scripts/GameScale.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+public class GameScale : MonoBehaviour {
+
+	[SerializeField]
+	int Step = 16;
+
+	void Start ()
+	{
+		Camera Cam = GetComponent<Camera>();
+		Cam.orthographicSize = Screen.height / Step / 2;
+	}
+}
