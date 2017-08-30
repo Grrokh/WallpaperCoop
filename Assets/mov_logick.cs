f3ac715d8777c184eacbbb4982cb16cf64386f6f
Cooderer <vitalii_en@mail.ru>
2017-08-30 18:37:05 +0300

    2
    2

--- Assets/mov_logick.cs
+++ Assets/mov_logick.cs
@@ -3,24 +3,73 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class mov_logick : MonoBehaviour {
-
+    public int hp;
+    public bool Naprav;
+    public  string enemy_name;
+    public  GameObject enemy;
     int numb = 0;
     Vector3 start_pos;
+    Vector3 end_pos;
 	// Use this for initialization
 	void Start () {
-        start_pos=transform.position;
+        enemy_name = "";
+        start_pos = transform.position;
+        end_pos.x = 60;
+        end_pos.y = start_pos.y;
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void FixedUpdate () {
         if (Input.GetKeyDown(KeyCode.Escape))
         {
             Application.Quit();
         }
-        start_pos.x += Random.Range(0.1f, 0.3f);
-        transform.position=start_pos;
+        
+        start_pos = transform.position;
+        if (enemy != null)
+        {
+
+        }
+        else
+        {
+            if (Naprav)
+            {
+                start_pos.x += Random.Range(0.05f, 0.1f);
+            }
+            else
+            {
+                start_pos.x += Random.Range(-0.05f, -0.1f);
+            }
+            transform.position = start_pos;
+        }
+
+       
         numb++;
-        Debug.Log(numb+" X:" + start_pos.x);
-        Debug.Log(numb+" Y:" + start_pos.y);
     }
+     void OnTriggerEnter2D(BoxCollider2D col)
+    {
+        if (Naprav)
+        {
+            if (col.gameObject.tag == "Team2")
+            {
+                enemy = col.gameObject;
+
+            }
+        }
+        else
+        {
+            if (col.gameObject.tag == "Team1")
+            {
+                enemy = col.gameObject;
+
+            }
+        }
+
+    }
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        
+    }
+
 }
---
+++ Assets/trig_left.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class trig_left : MonoBehaviour {
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        /*if(col.gameObject.tag == "Team1")
+        {
+            Destroy(col.gameObject);
+        }*/
+    }
+    private void OnTriggerExit2D(Collider2D col)
+    {
+       
+            Destroy(col.gameObject);
+        
+    }
+    private void OnTriggerStay2D(Collider2D col)
+    {
+        
+    }
+}
